8a73cf896b6ae406eef769c5d09249fc3d96031f
Subba Reddy Veeramreddy <subbareddyv.uoh@gmail.com>
2021-05-18 01:53:12 +0530

    Update File Header with License
    Signed-off-by: Subba Reddy Veeramreddy <subbareddyv.uoh@gmail.com>

--- fakehtppclient.go
+++ fakehtppclient.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2021, Subba Reddy Veeramreddy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package fakehttpclient
 
 import (
@@ -24,11 +40,11 @@ type ResponseConfig struct {
 	// Max number of times that the client fetches response
 	// If MaxRun is greater than Length of Responses,
 	// then Responses will be cycled until MaxRus is elapsed
-	MaxRun    int
-	cursor    int
+	MaxRun int
+	cursor int
 }
 
-func (rc *ResponseConfig) handler() (*http.Response, error)  {
+func (rc *ResponseConfig) handler() (*http.Response, error) {
 	if rc.cursor >= rc.MaxRun {
 		return nil, errors.New("client exhausted")
 	}
