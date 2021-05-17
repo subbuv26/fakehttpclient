8f5192f4908c3dce4397e76134412a52f30a5337
Subba Reddy Veeramreddy <subbareddyv.uoh@gmail.com>
2021-05-18 00:51:40 +0530

    Add Feature HTTP Method based Responses
    Signed-off-by: Subba Reddy Veeramreddy <subbareddyv.uoh@gmail.com>

--- fakeclient.go
+++ fakeclient.go
@@ -10,46 +10,86 @@ type roundTripHandler struct {
 }
 
 type roundTripperContext struct {
-	responses []*http.Response
-	cursor    int
-	maxRun    int
+	respCfg ResponseConfigMap
 }
 
-func (rtc *roundTripperContext) RoundTrip(_ *http.Request) (*http.Response, error) {
-	if rtc.cursor >= rtc.maxRun {
+// ResponseConfigMap needs to be composed in order to utilise this Package
+// It is map where the Key is HTTP Method, that would be enclosed in HTTP request
+type ResponseConfigMap map[string]*ResponseConfig
+
+// ResponseConfig wraps a List of Responses
+type ResponseConfig struct {
+	// Responses that would be fetched by fake client for a continuous requests
+	Responses []*http.Response
+	// Max number of times that the client fetches response
+	// If MaxRun is greater than Length of Responses,
+	// then Responses will be cycled until MaxRus is elapsed
+	MaxRun    int
+	cursor    int
+}
+
+func (rc *ResponseConfig) handler() (*http.Response, error)  {
+	if rc.cursor >= rc.MaxRun {
 		return nil, errors.New("client exhausted")
 	}
 
-	index := rtc.cursor % len(rtc.responses)
-	rtc.cursor++
+	index := rc.cursor % len(rc.Responses)
+	rc.cursor++
 
-	if rtc.responses[index] == nil {
+	if rc.Responses[index] == nil {
 		return nil, errors.New("http: nil Request")
 	}
 
-	return rtc.responses[index], nil
+	return rc.Responses[index], nil
 }
 
-func (rth roundTripHandler) RoundTrip(req *http.Request) (*http.Response, error) {
-	return rth.context.RoundTrip(req)
-}
-
-func NewFakeHTTPClient(responses []*http.Response, maxRun int) (*http.Client, error) {
-	if len(responses) == 0 {
-		return nil, errors.New("empty list of responses")
+func (rtc *roundTripperContext) Handler(req *http.Request) (*http.Response, error) {
+	respCfg, ok := rtc.respCfg[req.Method]
+	if !ok {
+		return nil, errors.New("No Responses for HTTP Method: " + req.Method)
 	}
 
-	if maxRun <= 0 {
-		return nil, errors.New("non-positive maxRun")
+	return respCfg.handler()
+}
+
+// PutContext Validates the input and updates the roundTripperContext
+func (rtc *roundTripperContext) PutContext(responseMap ResponseConfigMap) error {
+	if len(responseMap) == 0 {
+		return errors.New("empty map of responses")
+	}
+
+	for k, v := range responseMap {
+		if v == nil || len(v.Responses) == 0 {
+			return errors.New("Empty Response list for HTTP Method: " + k)
+		}
+		if v.MaxRun < 0 {
+			return errors.New("Invalid MaxRun for HTTP Method: " + k)
+		}
+		if v.MaxRun == 0 {
+			v.MaxRun = len(v.Responses)
+		}
+	}
+	rtc.respCfg = responseMap
+
+	return nil
+}
+
+// RoundTrip is the implementation of http.RoundTripper interface
+func (rth roundTripHandler) RoundTrip(req *http.Request) (*http.Response, error) {
+	return rth.context.Handler(req)
+}
+
+// NewFakeHTTPClient returns a fake http client with the behaviour as configured in ResponseConfigMap
+func NewFakeHTTPClient(responseMap ResponseConfigMap) (*http.Client, error) {
+	rtc := &roundTripperContext{}
+	if err := rtc.PutContext(responseMap); err != nil {
+		return nil, err
 	}
 
 	client := &http.Client{}
 	client.Transport = roundTripHandler{
-		context: &roundTripperContext{
-			responses: responses,
-			cursor:    0,
-			maxRun:    maxRun,
-		},
+		context: rtc,
 	}
+
 	return client, nil
 }
--- fakeclient_test.go
+++ fakeclient_test.go
@@ -14,97 +14,138 @@ func TestFakeHTTPClient(t *testing.T) {
 	RunSpecs(t, "Fake HTTP Client Suite")
 }
 
-var _= Describe("Fake HTTP Client Tests", func() {
+var _ = Describe("Fake HTTP Client Tests", func() {
 	Describe("Getting New Client", func() {
-		var responses []*http.Response
+		var responseMap ResponseConfigMap
 		BeforeEach(func() {
-			responses = []*http.Response{
-				{
-					StatusCode: 200,
-					Header:     http.Header{},
-					Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
+			responseMap = make(ResponseConfigMap)
+			responseMap[http.MethodPost] = &ResponseConfig{
+				Responses: []*http.Response{
+					{
+						StatusCode: 200,
+						Header:     http.Header{},
+						Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
+					},
 				},
 			}
+
 		})
 
 		It("Create a client", func() {
-
-			client, err := NewFakeHTTPClient(responses, 2)
+			responseMap[http.MethodPost].MaxRun = 2
+			client, err := NewFakeHTTPClient(responseMap)
 			Expect(err).To(BeNil(), "Failed to Create Client")
 			Expect(client).NotTo(BeNil(), "Failed to Create Client")
 		})
 
 		It("Empty Responses", func() {
-			client, err := NewFakeHTTPClient(nil, 2)
-			Expect(err).NotTo(BeNil(), "Failed to Validate responses")
-			Expect(client).To(BeNil(), "Failed to Validate responses")
+			responseMap[http.MethodPost].MaxRun = 2
+			responseMap[http.MethodPost].Responses = nil
+			client, err := NewFakeHTTPClient(responseMap)
+			Expect(err).NotTo(BeNil(), "Failed to Validate Response Configuration")
+			Expect(client).To(BeNil(), "Failed to Validate Response Configuration")
 		})
 
 		It("Invalid maxRun (0)", func() {
-			client, err := NewFakeHTTPClient(responses, 0)
-			Expect(err).NotTo(BeNil(), "Failed to Validate responses")
-			Expect(client).To(BeNil(), "Failed to Validate responses")
+			client, err := NewFakeHTTPClient(responseMap)
+			Expect(err).To(BeNil(), "Failed to Create Client")
+			Expect(client).NotTo(BeNil(), "Failed to Create Client")
+			Expect(responseMap[http.MethodPost].MaxRun).To(Equal(1), "Failed to set MaxRun")
 		})
 
 		It("Invalid maxRun (-ve)", func() {
-			client, err := NewFakeHTTPClient(responses, -2)
-			Expect(err).NotTo(BeNil(), "Failed to Validate responses")
-			Expect(client).To(BeNil(), "Failed to Validate responses")
+			responseMap[http.MethodPost].MaxRun = -2
+			client, err := NewFakeHTTPClient(responseMap)
+			Expect(err).NotTo(BeNil(), "Failed to Validate Response Configuration")
+			Expect(client).To(BeNil(), "Failed to Validate Response Configuration")
+		})
+
+		It("Invalid maxRun (-ve)", func() {
+			client, err := NewFakeHTTPClient(nil)
+			Expect(err).NotTo(BeNil(), "Failed to Validate Response Configuration")
+			Expect(client).To(BeNil(), "Failed to Validate Response Configuration")
 		})
 	})
 
 	Describe("Send Requests with the Client", func() {
+		var responseMap ResponseConfigMap
 		var client *http.Client
 		var resp1, resp2, resp3 *http.Response
-		var req *http.Request
+		var postReq, getReq *http.Request
 
 		BeforeEach(func() {
 			resp1 = &http.Response{
-					StatusCode: 200,
-					Header:     http.Header{},
-					Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
-				}
+				StatusCode: 200,
+				Header:     http.Header{},
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
+			}
 
 			resp2 = &http.Response{
-					StatusCode: 404,
-					Header:     http.Header{},
-					Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
-				}
+				StatusCode: 404,
+				Header:     http.Header{},
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
+			}
 
 			resp3 = &http.Response{
-					StatusCode: 505,
-					Header:     http.Header{},
-					Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
-				}
+				StatusCode: 503,
+				Header:     http.Header{},
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("body"))),
+			}
 
-			req, _ = http.NewRequest("POST", "http://1.2.3.4",  bytes.NewBuffer([]byte("{}")))
+			postReq, _ = http.NewRequest("POST", "http://1.2.3.4", bytes.NewBuffer([]byte("{}")))
+			getReq, _ = http.NewRequest("GET", "http://1.2.3.4", bytes.NewBuffer([]byte("")))
+
+			responseMap = make(ResponseConfigMap)
 		})
 
 		It("Basic Request and Response", func() {
-			responses := []*http.Response{resp1}
-			client, _ = NewFakeHTTPClient(responses, 1)
-			resp, err := client.Do(req)
+			responseMap[http.MethodGet] = &ResponseConfig{}
+			responseMap[http.MethodGet].Responses = []*http.Response{resp1}
+
+			client, _ = NewFakeHTTPClient(responseMap)
+			resp, err := client.Do(getReq)
 			Expect(err).To(BeNil())
 			Expect(resp).To(Equal(resp1))
 		})
 
 		It("Continuous Requests and Responses", func() {
-			responses := []*http.Response{resp1, resp2, resp3}
-			client, _ = NewFakeHTTPClient(responses, 3)
-			for _, resp := range responses {
-				rsp, err := client.Do(req)
-				Expect(err).To(BeNil())
-				Expect(rsp).To(Equal(resp))
-			}
+			responseMap[http.MethodPost] = &ResponseConfig{}
+			responseMap[http.MethodPost].Responses = []*http.Response{resp1, resp3}
+			responseMap[http.MethodGet] = &ResponseConfig{}
+			responseMap[http.MethodGet].Responses = []*http.Response{resp1, resp2, resp3}
+
+			client, _ = NewFakeHTTPClient(responseMap)
+
+			rsp, err := client.Do(postReq)
+			Expect(err).To(BeNil())
+			Expect(rsp).To(Equal(resp1))
+
+			rsp, err = client.Do(getReq)
+			Expect(err).To(BeNil())
+			Expect(rsp).To(Equal(resp1))
+
+			rsp, err = client.Do(getReq)
+			Expect(err).To(BeNil())
+			Expect(rsp).To(Equal(resp2))
+
+			rsp, err = client.Do(postReq)
+			Expect(err).To(BeNil())
+			Expect(rsp).To(Equal(resp3))
+
+			rsp, err = client.Do(getReq)
+			Expect(err).To(BeNil())
+			Expect(rsp).To(Equal(resp3))
 		})
 
 		It("Run over multiple Cycles", func() {
-			responses := []*http.Response{resp1, resp2, resp3}
-			client, _ = NewFakeHTTPClient(responses, 9)
+			responseMap[http.MethodGet] = &ResponseConfig{}
+			responseMap[http.MethodGet].Responses = []*http.Response{resp1, resp2, resp3}
+			responseMap[http.MethodGet].MaxRun = 9
+			client, _ = NewFakeHTTPClient(responseMap)
 
 			for i := 0; i < 3; i++ {
-				for _, resp := range responses {
-					rsp, err := client.Do(req)
+				for _, resp := range responseMap[http.MethodGet].Responses {
+					rsp, err := client.Do(getReq)
 					Expect(err).To(BeNil())
 					Expect(rsp).To(Equal(resp))
 				}
@@ -112,22 +153,34 @@ var _= Describe("Fake HTTP Client Tests", func() {
 		})
 
 		It("Out Run maxRun", func() {
-			responses := []*http.Response{resp1, resp2, resp3}
-			client, _ = NewFakeHTTPClient(responses, 3)
-			for _, resp := range responses {
-				rsp, err := client.Do(req)
+			responseMap[http.MethodPost] = &ResponseConfig{}
+			responseMap[http.MethodPost].Responses = []*http.Response{resp1, resp2, resp3}
+			responseMap[http.MethodPost].MaxRun = 3
+			client, _ = NewFakeHTTPClient(responseMap)
+			for _, resp := range responseMap[http.MethodPost].Responses {
+				rsp, err := client.Do(postReq)
 				Expect(err).To(BeNil())
 				Expect(rsp).To(Equal(resp))
 			}
-			rsp, err := client.Do(req)
+			rsp, err := client.Do(postReq)
 			Expect(err).NotTo(BeNil())
 			Expect(rsp).To(BeNil())
 		})
 
 		It("Nil Response", func() {
-			responses := []*http.Response{nil}
-			client, _ = NewFakeHTTPClient(responses, 1)
-			resp, err := client.Do(req)
+			responseMap[http.MethodPost] = &ResponseConfig{}
+			responseMap[http.MethodPost].Responses = []*http.Response{nil}
+			client, _ = NewFakeHTTPClient(responseMap)
+			resp, err := client.Do(postReq)
+			Expect(err).NotTo(BeNil())
+			Expect(resp).To(BeNil())
+		})
+
+		It("No Responses for http Method", func() {
+			responseMap[http.MethodPost] = &ResponseConfig{}
+			responseMap[http.MethodPost].Responses = []*http.Response{resp1}
+			client, _ = NewFakeHTTPClient(responseMap)
+			resp, err := client.Do(getReq)
 			Expect(err).NotTo(BeNil())
 			Expect(resp).To(BeNil())
 		})
